96bc78d589eeca0659d15cde36f986f1a674ccfc
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-01-14 09:50:37 +0200

    adding new classes

---
+++ src/com/company/Person.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAge(int age){
+        this.age=age>0?age:0;
+    }
+    public boolean isTeen(){
+        return (age>12&&age<19) ? true:false;
+    }
+    public String getFullName(){
+        return (firstName.isEmpty() && lastName.isEmpty()) ? "" :
+                (firstName.isEmpty()) ? lastName :
+                        (lastName.isEmpty()) ? firstName : firstName + " " + lastName;
+        }
+
+}
